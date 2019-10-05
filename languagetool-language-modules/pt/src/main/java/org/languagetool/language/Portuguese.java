cc93461f0e475783ff832f3250017fa163471b02
Tiago F. Santos <tiagofsantos81@sapo.pt>
2019-10-05 15:52:19 +0100

    Revert "update"
    This reverts commit bd97aa2319139124836b03238309e1ca2c82b448.

--- languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/Portuguese.java
@@ -82,8 +82,8 @@ public class Portuguese extends Language implements AutoCloseable {
   public Contributor[] getMaintainers() {
     return new Contributor[] {
             new Contributor("Marco A.G. Pinto", "http://www.marcoagpinto.com/"),
-            new Contributor("Tiago F. Santos (3.6-4.7)", "https://github.com/TiagoSantos81"),
-            new Contributor("Matheus Poletto (pt-BR)", "https://github.com/MatheusPoletto")
+            new Contributor("Tiago F. Santos (3.6+)", "https://github.com/TiagoSantos81"),
+            new Contributor("Matheus Poletto (pt-BR) (3.5-4.3)", "https://github.com/MatheusPoletto")
     };
   }
 
