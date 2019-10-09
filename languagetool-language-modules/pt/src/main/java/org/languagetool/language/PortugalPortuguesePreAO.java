c3e86af9e766ed8adfc38c8587b8fd9dcb77df01
Tiago F. Santos <tiagofsantos81@sapo.pt>
2019-10-09 09:58:07 +0100

    [pt-PT-preAO] add pre-AO Portugal Portuguese locale
    * includes common rules;
    - hunspell dictionary based on Dicionários Complementares Portuguseses;
    - hiphenation database;
    - a few specific rules.

---
+++ languagetool-language-modules/pt/src/main/java/org/languagetool/language/PortugalPortuguesePreAO.java
@@ -0,0 +1,64 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2012 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.languagetool.Language;
+import org.languagetool.UserConfig;
+import org.languagetool.rules.*;
+import org.languagetool.rules.pt.*;
+
+/**
+ * @since 3.6
+ */
+public class PortugalPortuguesePreAO  extends Portuguese {
+  
+  @Override
+  public String[] getCountries() {
+    return new String[]{"PT-preAO"};
+  }
+
+  @Override
+  public String getName() {
+    return "Portuguese (Portugal preAO)";
+  }
+
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) throws IOException {
+    List<Rule> rules = new ArrayList<>();
+    rules.addAll(super.getRelevantRules(messages, userConfig, motherTongue, altLanguages));
+    rules.add(new PreReformPortugueseCompoundRule(messages));
+    rules.add(new PreReformPortugueseDashRule());
+    rules.add(new PortugalPortugueseReplaceRule(messages));
+    return rules;
+  }
+
+  @Override
+  public int getPriorityForId(String id) {
+    switch (id) {
+      case "PT_COMPOUNDS_PRE_REFORM":         return  1;
+      // case "PORTUGUESE_OLD_SPELLING_INTERNAL": return -9;
+    }
+    return super.getPriorityForId(id);
+  }
+}
