0c40123df18b9c80f39d385b0ab6e88029657702
Jim O'Regan <jaoregan@tcd.ie>
2019-10-15 23:29:30 +0200

    add some generic rules

--- languagetool-language-modules/ga/src/main/java/org/languagetool/language/Irish.java
+++ languagetool-language-modules/ga/src/main/java/org/languagetool/language/Irish.java
@@ -86,16 +86,25 @@ public class Irish extends Language implements AutoCloseable {
   public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) throws IOException {
     return Arrays.asList(
             new CommaWhitespaceRule(messages),
+	    new GenericUnpairedBracketsRule(messages,
+                    Arrays.asList("[", "(", "{", "\"", "“"),
+                    Arrays.asList("]", ")", "}", "\"", "”")),
             new DoublePunctuationRule(messages),
             new UppercaseSentenceStartRule(messages, this),
+	    new LongSentenceRule(messages, userConfig, -1, true),
+	    new LongParagraphRule(messages, this, userConfig),
+	    new UppercaseSentenceStartRule(messages, this),
             new MultipleWhitespaceRule(messages, this),
+	    new SentenceWhitespaceRule(messages),
+	    new WhiteSpaceBeforeParagraphEnd(messages, this),
+	    new WhiteSpaceAtBeginOfParagraph(messages),
+	    new ParagraphRepeatBeginningRule(messages, this),
 	    new WordRepeatRule(messages, this),
 	    new MorfologikIrishSpellerRule(messages, this, userConfig),
 	    new LogainmRule(messages),
 	    new PeopleRule(messages),
 	    new SpacesRule(messages),
-	    new CompoundRule(messages),
-	    new SentenceWhitespaceRule(messages)
+	    new CompoundRule(messages)
     );
   }
 
