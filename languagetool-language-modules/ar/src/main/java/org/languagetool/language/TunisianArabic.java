141f87e987421a313fe5f33414b6f862d8b7752b
Tiago F. Santos <tiagofsantos81@sapo.pt>
2019-11-24 11:07:34 +0000

    [ar ar-DZ ar-EG ar-SA ar-TN] add country locales to activate spellchecking
    * only added countries that already have a hunspell dictionary entry

---
+++ languagetool-language-modules/ar/src/main/java/org/languagetool/language/TunisianArabic.java
@@ -0,0 +1,61 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2012 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.languagetool.Language;
+import org.languagetool.UserConfig;
+import org.languagetool.rules.*;
+
+public class TunisianArabic extends Arabic {
+
+  @Override
+  public String getName() {
+    return "Arabic (Tunisia)";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[]{"TN"};
+  }
+
+/*
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) throws IOException {
+    List<Rule> rules = new ArrayList<>();
+    rules.addAll(super.getRelevantRules(messages, userConfig, motherTongue, altLanguages));
+    rules.add(new PostReformPortugueseCompoundRule(messages));
+    return rules;
+  }
+*/
+
+  /*
+  @Override
+  public int getPriorityForId(String id) {
+    switch (id) {
+      case "COMPOUNDS_POST_REFORM":              return  1;
+    }
+    return super.getPriorityForId(id);
+  }
+  */
+}
