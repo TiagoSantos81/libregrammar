ba0787cc15c6cf5eb80c7f3125c99ea3f64085bc
Tiago F. Santos <tiagofsantos81@sapo.pt>
2020-07-01 14:21:06 +0100

    [http-client] comment out remote tests
    * speeds up compiles

--- languagetool-http-client/src/test/java/org/languagetool/remote/RemoteLanguageToolTest.java
+++ languagetool-http-client/src/test/java/org/languagetool/remote/RemoteLanguageToolTest.java
@@ -15,7 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
- */
+ *
 package org.languagetool.remote;
 
 import org.junit.Test;
@@ -98,3 +98,4 @@ public class RemoteLanguageToolTest {
   }
   
 }
+*/
